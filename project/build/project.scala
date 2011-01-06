d5a7b05dfdc61220f06ac05f95efe71d2cf6dfeb
Remco Bos <remco_bos@yahoo.com>
2011-01-06 15:52:14 +0100

    Scalate 1.3.2

--- project/build/project.scala
+++ project/build/project.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 class ScalateCliProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.3"
+  val scalate = "org.fusesource.scalate" % "scalate-core" % "1.3.2"
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.6.1"
 
   override def mainClass = Some("net.stbbs.yasushi.ScalatePrecompiler")
